ebc701f8601f74fe0ed894dcac9131b61ee916bd
Yaroslav Guz <aroslavguz@gmail.com>
2025-07-03 11:36:45 +0300

    Init kafka producer

---
+++ patient-service/src/main/java/org/example/patientservice/kafka/KafkaProducer.java
@@ -0,0 +1,11 @@
+package org.example.patientservice.kafka;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaProducer {
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+}
